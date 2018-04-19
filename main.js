ddeabd0016cf194371ab697747d85e4d80113585
fengjb <18794758986@163.com>
2018-04-19 17:55:28 +0800

    add express-session

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import query from './src/mysql.js'
+import session from 'express-session'
+import parseurl from 'parseurl'
+
 var app = express()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
+
+app.use(session({//todo : export
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+ 
+app.use(function (req, res, next) {
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+ 
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+ 
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+ 
+  next()
+})
+
 app.get('/', function (req, res) {
-  res.end('hello')
+  // res.end('hello')
+  console.log('req.session.views',req.session.views['/'])
+  res.send('you viewed this page ' + req.session.views['/'] + ' times')
 })
 
 app.all('*', function(req, res, next) {
@@ -17,7 +44,7 @@ app.all('*', function(req, res, next) {
   next()
 })
 
-app.post('/process_post', urlencodedParser, function (req, res,next) {
+app.post('/process_post', urlencodedParser, function (req, res,next) {//todo: export
   let sql = `SELECT * from dat_user where user_name = "${req.body.name}" and password = "${req.body.pwd}" `
   console.log('sql',sql)
   query(sql, function (error, results, fields) {
