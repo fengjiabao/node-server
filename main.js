16518d59707a5a4dade3baaea6aa36a60529581e
fengjb <18794758986@163.com>
2018-04-19 15:42:39 +0800

    standard code

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// var http = require('http')
-
-// http.createServer(function (request, response) {
-
-//     // 发送 HTTP 头部
-//     // HTTP 状态值: 200 : OK
-//     // 内容类型: text/plain
-//     response.writeHead(200, {'Content-Type': 'text/plain'}); //text/plain: 纯文本格式；text/html:html格式；text/xml;text/jpeg、png\gif; application/json：json数据格式；multipart/form-data ： 需要在表单中进行文件上传时，就需要使用该格式
-
-//     // 发送响应数据 "Hello World"
-//     response.end('Hello World\n');
-// }).listen(8888);
-
-// // 终端打印如下信息
-// console.log('Server running at http://127.0.0.1:8888/');
 import express from 'express'
 import bodyParser from 'body-parser'
 import query from './src/mysql.js'
@@ -23,15 +8,23 @@ app.get('/', function (req, res) {
   res.end('hello')
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {
-      // 输出 JSON 格式
-  var response = {
-    'first_name': req.body.first_name,
-    'last_name': req.body.last_name
-  }
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  console.log('response', response)
-  res.end(JSON.stringify(response))
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next()
+})
+
+app.post('/process_post', urlencodedParser, function (req, res,next) {
+  let sql = `SELECT * from dat_user where user_name = "${req.body.name}" and password = "${req.body.pwd}" `
+  console.log('sql',sql)
+  query(sql, function (error, results, fields) {
+    if (error) throw error
+    let code = results[0] ? 1 : -1,msg = {code: code} 
+    res.end(JSON.stringify(msg))
+  })
 })
 
 var server = app.listen(8888, function () {
@@ -40,9 +33,7 @@ var server = app.listen(8888, function () {
 
   console.log('应用实例，访问地址为 http://%s:%s', host, port)
 })
+
 console.log('Server running at http://127.0.0.1:8888/')
 
-query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error
-  console.log('The solution is: ', results[0].solution)
-})
+
